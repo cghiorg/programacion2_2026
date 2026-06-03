57e206177804047f0a2d612a9f30141eccfc08c8
carlos <cghiorg@gmail.com>
2026-06-03 19:16:14 -0300

    Poo

--- programacion POO/poo3.py
+++ programacion POO/poo3.py
@@ -6,18 +6,37 @@ class CuentaBancaria:
     def depositar(self, monto):
         self.__saldo += monto
 
+    def retirar(self, monto):
+        if monto <= self.__saldo:
+            self.__saldo -= monto
+        else:
+            print("Saldo insuficiente")
+
     def ver_saldo(self):
         return self.__saldo
-    
-    
-cuenta1 = CuentaBancaria("Juan", 50000)
+
+    def transferir(self, otra_cuenta, monto):
+        if monto <= self.__saldo:
+            self.__saldo -= monto
+            otra_cuenta.__saldo += monto
+            print(self.titular, "le transfirió $", monto, "a", otra_cuenta.titular)
+        else:
+            print("Saldo insuficiente")
+
+
+cuenta1 = CuentaBancaria("Juan", 20000)
+cuenta2 = CuentaBancaria("Ana", 20000)
 
 print(cuenta1.titular)
-
 print(cuenta1.ver_saldo())
 
 cuenta1.depositar(10000)
-
 print(cuenta1.ver_saldo())
 
-cuenta1.ver_saldo()
+cuenta1.retirar(5000)
+print(cuenta1.ver_saldo())
+
+cuenta1.transferir(cuenta2, 2500)
+
+print("Saldo de Juan:", cuenta1.ver_saldo())
+print("Saldo de Ana:", cuenta2.ver_saldo())
